56af55cd97479dec263acaf433746d40c30186a7
MeyfroidtArthur <arthur.meyfroidt@student.howest.be>
2025-09-30 11:32:56 +0200

    feat: Remove sensitive configuration files including access tokens and Firebase options and added template

--- lib/config.dart
+++
@@ -1,6 +0,0 @@
-class Config {
-  static const String mapboxAccessToken =
-      "pk.eyJ1IjoiYXJ0aHVybWV5ZnJvaWR0IiwiYSI6ImNtYTNseDMyajE2MzYyaXNmN2pxZmRqZ2EifQ.vV10oj7eLmsUgyAU9zb0sQ";
-  static const String openRouteServiceApiKey =
-      "5b3ce3597851110001cf6248df8b8f5c778e4284921481a807469217";
-}
